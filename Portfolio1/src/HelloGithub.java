ac97b7452476a35c6e41f935124b9caafcc26ed4
jonatan-student <78746701+jonatan-student@users.noreply.github.com>
2022-02-23 15:28:50 +0100

    githup update test

--- Portfolio1/src/HelloGithub.java
+++ Portfolio1/src/HelloGithub.java
@@ -1,5 +1,5 @@
 public class HelloGithub {
     public static void main(String[] args) {
-        System.out.println("hello GitHub");
+        System.out.println("hello GitHub" + "sup home biscuit");
     }
 }
