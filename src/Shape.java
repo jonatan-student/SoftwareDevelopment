5af54c5e8b41c727b63e39813edf03974918c086
jonatan-student <78746701+jonatan-student@users.noreply.github.com>
2022-03-02 15:45:08 +0100

    created shape class

---
+++ src/Shape.java
@@ -0,0 +1,14 @@
+public abstract class Shape {
+    public abstract double GetArea();
+
+    public abstract double GetCircumference();
+
+    public abstract Vector center();
+
+    public abstract boolean containsPoint(Vector point);
+
+    public double dist(Shape shape){
+        Vector dist = this.center().sub(shape.center());
+        return dist.length();
+    }
+}
